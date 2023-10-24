5d8686348e72f3aed9b329920613a2ae9b5aaee6
jennifer godfrey <jmarthagodfrey@gmail.com>
2023-10-24 09:24:24 -0600

    update code

--- 8kyu-8.js
+++ 8kyu-8.js
@@ -6,10 +6,6 @@
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
 var summation = function (num) {
-    if (num <= 0) {
-        return 0;
-    }
-
     let sum = 0;
     for (let i = 1; i <= num; i++) {
         sum += i;
