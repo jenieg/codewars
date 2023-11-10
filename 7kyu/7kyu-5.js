bb20ea6a878a758e9fb625ee8f055b8282da73aa
Jennifer <jmarthagodfrey@gmail.com>
2023-11-10 09:43:37 -0700

    codewars

---
+++ 7kyu/7kyu-5.js
@@ -0,0 +1,20 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    const arr = str.split('');
+    let count = 0;
+    arr.forEach(i => {
+        if (i === 'a' ||
+            i === 'e' ||
+            i === 'i' ||
+            i === 'o' ||
+            i === 'u') {
+                count += 1;
+            }
+    });
+    return count;
+}
