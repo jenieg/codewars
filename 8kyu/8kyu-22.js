6bf10b28394e2bf32d6a87c1cd0e01728d9040f9
Jennifer <jmarthagodfrey@gmail.com>
2023-12-13 11:04:40 -0700

    codewars

--- 8kyu/8kyu-22.js
+++ 8kyu/8kyu-22.js
@@ -1,5 +1,5 @@
 // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
 function makeNegative(num) {
-    return Math.abs(num) * -Math.sign(num)
+    return num < 0 ? num : -Math.abs(num);
 }
